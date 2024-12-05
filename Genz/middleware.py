cb688f41fb31171b79cf077373d01272a13e5c5a
Hasan Yusuf Khan <116283180+YusufKhan2016@users.noreply.github.com>
2024-12-05 19:18:11 +0600

    middlware check

--- Genz/middleware.py
+++ Genz/middleware.py
@@ -1,4 +1,7 @@
 # Genz/middleware.py
+import logging
+
+logger = logging.getLogger(__name__)
 
 from django.http import HttpResponsePermanentRedirect
 
@@ -7,6 +10,7 @@ class RedirectToWwwMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        logger.info(f"Request host: {request.get_host()}")
         host = request.get_host()
         if host == "gen-zit.com":
             return HttpResponsePermanentRedirect("https://www.gen-zit.com" + request.path)
